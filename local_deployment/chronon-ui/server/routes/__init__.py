2fc091ad18aa245e85bedaae5dbd1b594ebf93e3
Dustin Hamerla <dhamerla@salesforce.com>
2025-11-03 12:40:41 +0100

    Kinesis routes in ui-server to create simple web ui for kensis

--- local_deployment/chronon-ui/server/routes/__init__.py
+++ local_deployment/chronon-ui/server/routes/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 from .v1.graph import router as graph_router
 from .v1.spark_data import router as spark_data_router
 from .v1.actions import router as actions_router
+from .v1.kinesis import router as kinesis_router
 
 router = APIRouter()
 router.include_router(graph_router)
 router.include_router(spark_data_router)
 router.include_router(actions_router)
+router.include_router(kinesis_router)
