b5ca6b3e13648e5f1bd7564bdde9b686545e26ae
Dustin Hamerla <dhamerla@salesforce.com>
2026-01-13 11:47:36 -0500

    add ExternalSourceRegistry

--- cloud_aws/src/main/scala/ai/chronon/integrations/aws/AwsApiImpl.scala
+++ cloud_aws/src/main/scala/ai/chronon/integrations/aws/AwsApiImpl.scala
@@ -48,12 +48,13 @@ class AwsApiImpl(conf: Map[String, String]) extends Api(conf) {
     * a fully functional Chronon serving stack in Aws
     * @return
     */
-  override def externalRegistry: ExternalSourceRegistry = ???
+  @transient lazy val registry: ExternalSourceRegistry = new ExternalSourceRegistry()
+  override def externalRegistry: ExternalSourceRegistry = registry
 
   /** The logResponse method is currently unimplemented. We'll need to implement this prior to bringing up the
     * fully functional serving stack in Aws which includes logging feature responses to a stream for OOC
     */
-  override def logResponse(resp: LoggableResponse): Unit = ???
+  override def logResponse(resp: LoggableResponse): Unit = ()
 
   override def genMetricsKvStore(tableBaseName: String): KVStore = {
     new DynamoDBKVStoreImpl(ddbClient, conf)
